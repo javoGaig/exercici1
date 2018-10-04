150100507db4b34318ced83a22b9ee247988e6c5
Javier Gaig <javier.gaig@students.salle.url.edu>
2018-10-04 13:08:35 +0200

    Main.c implementation done
    Main.c code implementation of the bidirectional queue that orders its
    numbers descendently

--- ex1/ex1/main.c
+++ ex1/ex1/main.c
@@ -1,7 +1,44 @@
+#include "LISTA.H"
 #include <stdio.h>
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
+int main(void){
+	int num, num_consulta, o=1, trobat=0;
+	lbo l;
+	
+	l = LlistaBidOrd_crea();
+	while (o){
+		printf ("\n\nQuieres ingresar un nuevo numero?\n");
+		printf ("SI=1 / NO=0\n\n");
+		scanf ("%d", &o);
+		if (o==1){
+			printf("\nNumero? ->  ");
+			scanf ("%d", &num);
+			if (LlistaBidOrd_buida(l)){
+				LlistaBidOrd_insereixDavantOrdenat(&l, num);
+			}else{
+				LlistaBidOrd_Vesinici(&l);
+				trobat=0;
+				while (!trobat && !LlistaBidOrd_fi(l)){
+					num_consulta = LlistaBidOrd_consulta (l);
+					if (num_consulta>num){
+						LlistaBidOrd_avanza(&l);
+					}else{
+						trobat=1;
+						LlistaBidOrd_insereixDavantOrdenat(&l, num);
+					}
+				}
+				if (!trobat){
+					LlistaBidOrd_retrocedeix(&l);
+					LlistaBidOrd_insereixDarrereOrdenat(&l, num);
+				}
+			}
+		}
+		printf ("\nNumeros introducidos ordenados de forma decreciente:\n");
+		LlistaBidOrd_Vesinici(&l);
+		while (!LlistaBidOrd_fi(l)){
+			printf("%d / ", LlistaBidOrd_consulta (l));
+			LlistaBidOrd_avanza(&l);
+		}
+	}
 	return 0;
 }
